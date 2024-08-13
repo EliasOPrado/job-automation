b27cdea127ab3eefb5990ee1c04fe69714bcd9b3
EliasOPrado <eliaspradoprofessional@outlook.com>
2024-08-13 19:47:38 -0300

    feat: Added model that will receive session id.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class LinkedInSession(models.Model):
+    email = models.EmailField()  # Store the associated email to link session cookies
+    cookies = models.JSONField()  # Store cookies as JSON
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Session for {self.email} - Created at {self.created_at}"
