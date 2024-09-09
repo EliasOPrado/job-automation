bc577535c80fa3f1191a2ba447c846c85f76b28d
EliasOPrado <eliaspradoprofessional@outlook.com>
2024-09-09 16:02:38 -0300

    chore: Added initial implementation

--- job_application_automation/urls.py
+++ job_application_automation/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('jobs/', include('core.urls')),
+    path('', include('job_platform.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_URL)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
