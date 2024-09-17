b51619894ba52bd0b176a317bfee826dfa09dcc7
EliasOPrado <eliaspradoprofessional@outlook.com>
2024-09-17 01:32:04 -0300

    Feat: Added initial template inheritance

--- job_platform/urls.py
+++ job_platform/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import HomeView
+from .views import HomeView, SignUpView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home-page'),
+    path('sign-up/', SignUpView.as_view(), name='signup-page'),
 ]
--- job_platform/views.py
+++ job_platform/views.py
@@ -4,11 +4,15 @@ from django.views import View
 # Create your views here.
 class HomeView(View):
     template_name = 'home.html'
+    context = {'css_file':'home_page.css'}
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name, self.context)
 
 class SignUpView(View):
-    pass
+    template_name = 'signup.html'
+    context = {'css_file':'signup_page.css'}
+    def get(self, request):
+        return render(request, self.template_name, self.context)
 
 class LoginView(View):
     pass
